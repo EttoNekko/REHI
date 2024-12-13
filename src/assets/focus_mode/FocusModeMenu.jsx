27582131f1e8dd03fb486d2409ceaceb834d43e2
EttoNekko <cillbyphertang@gmail.com>
2024-12-13 22:57:56 +0700

    add tunnel mode

--- src/assets/focus_mode/FocusModeMenu.jsx
+++ src/assets/focus_mode/FocusModeMenu.jsx
@@ -5,20 +5,39 @@ import simpleModeToggle from '../../features/simple_mode/simple_mode';
 const FocusModeMenu = ({ resetToggle, setResetToggle }) => {
   const [isTunnelModeActive, setIsTunnelModeActive] = useState(false);
   const [isSimpleModeActive, setIsSimpleModeActive] = useState(false);
+  const simpleOnMessage = { messageId: 'toggleVisor', active: 'on' };
+  const simpleOffMessage = { messageId: 'toggleVisor', active: 'off' };
 
   useEffect(() => {
-    chrome.storage.local.get(['isSimpleModeActive'], (result) => {
+    chrome.storage.local.get(['isSimpleModeActive', 'isTunnelModeActive'], (result) => {
       if (result.isSimpleModeActive !== undefined) {
         setIsSimpleModeActive(result.isSimpleModeActive);
         if (result.isSimpleModeActive) {
           simpleModeToggle();
         }
       }
+      if (result.isTunnelModeActive !== undefined) {
+        setIsTunnelModeActive(result.isTunnelModeActive);
+        if (result.isTunnelModeActive) {
+          const message = { messageId: 'toggleVisor', active: 'on' };
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, message);
+          });
+        }
+      }
     });
   }, []);
 
   const handleTunnelModeChange = () => {
-    setIsTunnelModeActive((prev) => !prev);
+    setIsTunnelModeActive((prev) => {
+      const newState = !prev;
+      const message = newState ? simpleOnMessage : simpleOffMessage;
+      chrome.storage.local.set({ isTunnelModeActive: newState });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message);
+      });
+      return newState;
+    });
   };
 
   const handleSimpleModeChange = () => {
@@ -32,10 +51,13 @@ const FocusModeMenu = ({ resetToggle, setResetToggle }) => {
 
   useEffect(() => {
     if (resetToggle) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { messageId: 'toggleVisor', active: 'off' });
+      });
       setIsTunnelModeActive(false);
       if (isSimpleModeActive) simpleModeToggle();
       setIsSimpleModeActive(false);
-      chrome.storage.local.set({ isSimpleModeActive: false });
+      chrome.storage.local.set({ isSimpleModeActive: false, isTunnelModeActive: false });
       setResetToggle(false);
     }
   }, [resetToggle]);
